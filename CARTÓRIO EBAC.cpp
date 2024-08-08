cb312b3b12d2be56fd758aae89a3c5471e6172dc
aoanjos <oliveiradosanjosandre@gmail.com>
2024-08-08 18:20:26 -0300

    Opção de saída
    Incluso no sistema a opção de saída do usuário.

--- CARTÓRIO EBAC.cpp
+++ CARTÓRIO EBAC.cpp
@@ -132,6 +132,7 @@ int main()
 		printf("\t1 - Registra nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("opcao: "); 
 		//Fim do menu
 		
@@ -151,6 +152,11 @@ int main()
 			
 			case 3:
 			deletar(); //Chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
